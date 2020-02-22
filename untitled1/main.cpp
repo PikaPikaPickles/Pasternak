4689c785235deb7471dbc0da2e91f184451fa36a
PikaPikaPickles <ybojmatov@yandex.ua>
2020-02-23 02:57:12 +0300

    Gaz

--- untitled1/main.cpp
+++ untitled1/main.cpp
@@ -1,20 +1,46 @@
 #include <iostream>
 
 using namespace std;
-class stack
-    {
-    public :
-    stack (int size ) ;
-    ~stack ( ) ;
-    void push (int a) ;
-    int pop ( ) ;
-    void clear ( ) ;
-    private :
-    int size ;
-    int ∗data ;
-    int top;
-    };
+class GasHolder
+{
+public:
+    double v1,m2,m1,T,P;
+    // Создать газгольдер заданного объёма.
+    // Температура созданного термостата равна 273 К.
+    GasHolder(float v){v1 =v;};
 
+    // Уничтожить газгольдер.
+
+    // Впрыск порции газа массой m и молярной массой M.
+    // Считать, что газ принимает текущую температуру газгольдера за пренебрежимо малое время.
+    void inject(float m, float M){m1=m; m2=M; };
+
+    // Подогреть газгольдер на dT градусов.
+    // Считать, что нагрев возможен до любых значений температуры.
+    void heat(float dT){T = T + dT;};
+
+    // Охладить газгольдер на dT градусов.
+    // При попытке охладить ниже 0 К температура становится ровно 0 К.
+    void cool(float dT){if ( T > 0 )T = T - dT;};
+
+    // Получить текущее давление в газгольдере.
+    // Считать, что для газа верно уравнение состояния PV = (m/M)RT.
+    // Значение постоянной R принять 8.31 Дж/(моль*К).
+    float getPressure(){ P = m1*8.31*T/(m2*v1);
+        return P ;};
+};
 int main()
-{ return 0;
+{GasHolder h(1.0);
+    h.inject(29, 29);
+    cout << "Pressure after operation: " << h.getPressure() << " Pa" << endl;
+    h.inject(29, 29);
+    cout << "Pressure after operation: " << h.getPressure() << " Pa" << endl;
+    h.heat(273);
+    cout << "Pressure after operation: " << h.getPressure() << " Pa" << endl;
+    h.cool(373);
+    cout << "Pressure after operation: " << h.getPressure() << " Pa" << endl;
+    h.cool(373);
+    cout << "Pressure after operation: " << h.getPressure() << " Pa" << endl;
+
+    return 0;
 }
